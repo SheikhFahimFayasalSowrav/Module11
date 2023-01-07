f3d1898ccfc440d3f5e3d977141fb2ec2581e812
Sheikh Fahim Faysal Sowrav <gfahim38@gmail.com>
2023-01-07 18:20:24 +0600

    Module 11 2nd Batch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-'''num = int(input())
+'''
+num = int(input())
 def fib(a,b,c):
    if c == 1:
        print(a)
@@ -12,10 +13,11 @@ if num <= 0:
 	print("Please enter a positive integer")
 else:
 	fib(0,1,num)
-
-
+'''
+'''
 import numpy as np
-
+'''
+'''
 list1 = [1,2,3,4]
 list2 = [5,6,7,8]
 list3 = [9,10,11,12]
@@ -25,12 +27,15 @@ print(array1)
 
 array2 = np.transpose(array1)
 print(array2)
-
+'''
+'''
 nums = {1, 2, 1, 3, 1, 4, 5, 6}
 print(nums)
 nums.add(-7)
 nums.remove(3)
 print(nums)
+'''
+'''
 
 first = {1, 2, 3, 4, 5, 6}
 second = {4, 5, 6, 7, 8, 9}
@@ -40,31 +45,34 @@ print(first & second)
 print(first - second)
 print(second - first)
 print(first ^ second)
-
+'''
+'''
 from itertools import chain, accumulate, takewhile
 nums = list(accumulate(range(8)))
 print (nums)
 
 print(list(takewhile(lambda x: x<= 6, nums)))
-
+'''
+'''
 from itertools import product, permutations
 letters = ("A", "B")
 print(list(product(letters, range(2))))
 print(list(permutations(letters)))
-
+'''
+'''
 num_set = {1, 2, 3, 4, 5}
 word_set = set(["spam", "eggs", "sausage"])
 print(3 in num_set)
 print("spam" not in word_set)
-
+'''
+'''
 from itertools import count
 for i in count(3):
     print(i)
     if i >=10:
         break
 
-
-print("Test for github")'''
+'''
 
 
 
